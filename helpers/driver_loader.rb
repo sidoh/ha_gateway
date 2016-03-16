e1f6e96a70b327d1cb4a32124a2b0c14ff8227ce
Christopher <cmullins@liveramp.com>
2016-03-16 00:18:34 -0700

    add driver loader helper

---
+++ helpers/driver_loader.rb
@@ -0,0 +1,26 @@
+require_relative 'config_provider'
+
+module HaGateway
+  module DriverLoader
+    include HaGateway::ConfigProvider
+
+    def build_driver(type, key)
+      devices = config_value(type)
+
+      if devices.nil? or devices[key].nil?
+        raise RuntimeError, "The #{type} \"#{key}\" is not defined. Add it to the config."
+      end
+
+      device = devices[key]
+      driver = device[:driver]
+
+      begin
+        require_relative "../drivers/#{type}/#{driver}"
+      rescue LoadError => e
+        raise RuntimeError, "Undefined driver type: #{type}/#{driver}"
+      end
+
+      Object.const_get(driver.upcase).new(params)
+    end
+  end
+end
